baf804dd55d6add040bd33af1b95d0e0204176e6
krisna12217016 <12217016@bsi.ac.id>
2021-09-26 15:51:03 +0700

    modif simpan

--- application/views/bio.php
+++ application/views/bio.php
@@ -1,8 +1,9 @@
 <?php
-class bio extends CI_Controller 
 {
-    public function index()
-    {
-        echo "<hi> Data Diri </h1>";
-    }
+    echo "<hi> Data Diri </h1>";
+    echo "Nama : Krisna Heru Saputra <br>
+        Tempat Lahir : Bekasi <br>
+        Tanggal Lahir : 23 Agustus 1994 <br>
+        Tempat Tinggal : Rawalumbu <br>
+        Motivasi Kuliah : Upgrade Keahlian";
 }
