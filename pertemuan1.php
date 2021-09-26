11505b3ba840d00b30d3342e921e15480d614117
krisna12217016 <12217016@bsi.ac.id>
2021-09-26 13:56:45 +0700

    modif simpan

---
+++ pertemuan1.php
@@ -0,0 +1,10 @@
+<?php
+class pertemuan1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1> MyBio </h1>";
+        echo "Nama :Krisna Heru <br>
+        Tempat Lahir : Bekasi";
+    }
+}
